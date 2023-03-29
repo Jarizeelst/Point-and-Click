ee6ff9a5318a0b9e1479bf03607c401d907009a5
Jarizeelst <jari.mol@sintlucasedu.nl>
2023-03-29 16:38:18 +0200

    code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 document.getElementById("mainTitle").innerText= "Point and Click adveture";
+
+const offsetCharacter = 16;
+const gameWindow = document.getElementById("gameWindow");
+const mainCharacter = document.getElementById("mainCharacter");
+
+gameWindow.onclick = function(e){
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+
+    mainCharacter.style.left = x - offsetCharacter +"px";
+    mainCharacter.style.top = y - offsetCharacter + "px";
+
+
+    console.log(e.target.id);
+    switch(e.target.id){
+        case"chest":
+        console.log("this is locked or do you have a key?");
+        break;
+        default: 
+        console.log("What can i do here?");
+    }
+}
+
+function showMessage(){
+    
+}

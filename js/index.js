54be1e00ecafa0fbb3643a0ac63997e45df04d6c
Jarizeelst <jari.mol@sintlucasedu.nl>
2023-04-05 16:31:35 +0200

    1

--- js/index.js
+++ js/index.js
@@ -1,28 +1,62 @@
-document.getElementById("mainTitle").innerText= "Point and Click adveture";
-
+document.getElementById("mainTitle").innerText = "Point and Click adventure";
 const offsetCharacter = 16;
+const sec = 1000;
 const gameWindow = document.getElementById("gameWindow");
 const mainCharacter = document.getElementById("mainCharacter");
+const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
 
-gameWindow.onclick = function(e){
-    var rect = gameWindow.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
+const counterSpeech = document.getElementById("counterSpeech");
+const counterCharacter = document.getElementById("counterCharacter");
 
-    mainCharacter.style.left = x - offsetCharacter +"px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
+const characterAudio = document.getElementById("characterAudio");
+const counterAudio = document.getElementById("counterAudio");
 
+gameWindow.onclick = function (e) {
+    if (mainCharacterSpeech.style.opacity == 0) {
+        var rect = gameWindow.getBoundingClientRect();
+        var x = e.clientX - rect.left; //x position within the element.
+        var y = e.clientY - rect.top;  //y position within the element.
 
-    console.log(e.target.id);
-    switch(e.target.id){
-        case"chest":
-        console.log("this is locked or do you have a key?");
-        break;
-        default: 
-        console.log("What can i do here?");
+        mainCharacter.style.left = x - offsetCharacter + "px";
+        mainCharacter.style.top = y - offsetCharacter + "px";
+
+        console.log(e.target.id);
+        switch (e.target.id) {
+            case "chest":
+                showMessage(mainCharacterSpeech, characterAudio, "this chest is closed.. DAMN YOU CHEST!");
+                break;
+            case "statue":
+                showMessage(mainCharacterSpeech, characterAudio, "ow boring statue...");
+                setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
+                setTimeout(showMessage, 4 * sec, counterSpeech, counterAudio, "No you are boring stupid!");
+                setTimeout(showMessage, 8 * sec, mainCharacterSpeech, characterAudio, "Ow I am so sorry. <br>I didn't know you could talk.");
+                setTimeout(function () { counterCharacter.style.opacity = 0; }, 12 * sec);
+                break;
+            default:
+                hideMessage(mainCharacterSpeech, characterAudio);
+                hideMessage(counterSpeech, counterAudio);
+                break;
+        }
     }
 }
 
-function showMessage(){
-    
+function showMessage(targetBubble, targetAudio, message) {
+    targetAudio.currentTime = 0;
+    targetAudio.play();
+    targetBubble.innerHTML = message;
+    targetBubble.style.opacity = 1;
+    setTimeout(hideMessage, 4 * sec, targetBubble, targetAudio);
 }
+
+function hideMessage(targetBubble, targetAudio) {
+    targetAudio.pause();
+    targetBubble.innerHTML = "...";
+    targetBubble.style.opacity = 0;
+}
+
+
+
+
+
+  
+    
